7dc17687e1efeec22f4ca60f6af83e006a5ed3a6
Ina Nivald <inanivald@hotmail.com>
2020-06-16 13:58:48 +0200

    Managed to get the game to send out virus on random position to both users with a random delay.

--- controllers/socket_controller.js
+++ controllers/socket_controller.js
@@ -1,36 +1,56 @@
 const users = {};
+let io = null;
 
-function getOnlineUsers() {
+
+function getOnlinePlayers() {
 	return Object.values(users);
 }
 
+function randomPositions(y, x) {
+	
+	let onlinePlayers = getOnlinePlayers();
+	console.log('onlinePlayers', onlinePlayers)
+	
+	if (onlinePlayers.length === 2){
+		const randomY = y * Math.random()
+		const randomX = x * Math.random()
+		const seconds = Math.floor(Math.random() * 10)
+		console.log(seconds)
+		setTimeout(function () {
+			io.emit('virus-positions', randomY, randomX)
+		}, seconds * 1000); ;
+	}
+
+}
+
 function handleUserDisconnect() {
-    
-	// broadcast to all connected sockets that this user has left the chat
 	if (users[this.id]) {
 		this.broadcast.emit('user-disconnected', users[this.id]);
 	}
+	delete users[this.id];
 
-	// remove user from list of connected users
-    delete users[this.id];
-
-    this.broadcast.emit('online-players', getOnlineUsers());
+    this.broadcast.emit('online-players', getOnlinePlayers());
 }
 
 function handleRegisterUser(username, callback) {
 	users[this.id] = username;
 	callback({
-		joinChat: true,
+		joinGame: true,
 		usernameInUse: false,
-		onlineUsers: getOnlineUsers(),
+		onlineUsers: getOnlinePlayers(),
 	});
 
-	this.emit('online-players', getOnlineUsers());
+	this.broadcast.emit('online-players', getOnlinePlayers());
 }
 
 module.exports = function(socket) {
 
+	io = this;
 	socket.on('disconnect', handleUserDisconnect);
 
     socket.on('register-player', handleRegisterUser);
+
+    socket.on('positions', randomPositions)
+   
 }
+
--- public/assets/js/game.js
+++ public/assets/js/game.js
@@ -3,11 +3,17 @@ const socket = io();
 const startEl = document.querySelector('#start');
 const gameWrapperEl = document.querySelector('#game-wrapper');
 const usernameForm = document.querySelector('#username-form');
+const play = document.querySelector('#play');
+const gameboard = document.querySelector('#gameboard');
+const x = 600;
+const y = 400;
+const virus = document.createElement('img');
 
 let username = null;
 
 const updateOnlinePlayers = (users) => {
     document.querySelector('#online-players').innerHTML = users.map(user => `<li class="user">${user}</li>`).join("");
+
 }
 
 usernameForm.addEventListener('submit', e => {
@@ -15,13 +21,32 @@ usernameForm.addEventListener('submit', e => {
 
 	username = document.querySelector('#username').value;
 	socket.emit('register-player', username, (status) => {
-        if (status.joinChat) {
+        if (status.joinGame) {
             startEl.classList.add('hide');
             gameWrapperEl.classList.remove('hide');
+
+            updateOnlinePlayers(status.onlineUsers);
         }
     })
 });
 
+const playGame = (randomY, randomX) => {
+    console.log('Random positions:', randomY, randomX)
+
+    virus.src = "https://fed19.thehiveresistance.com/wp-content/uploads/2020/06/a.svg";
+    virus.style.position = "absolute";
+    virus.style.top = randomY + 'px';
+    virus.style.left = randomX + 'px';
+    gameboard.appendChild(virus);  
+}
+
+socket.on('virus-positions', (randomY, randomX) => {
+    playGame(randomY, randomX)
+})
+
 socket.on('online-players', (users) => {
-	updateOnlinePlayers(users);
+    updateOnlinePlayers(users);
+    
+        socket.emit('positions', y, x)
+        
 });
